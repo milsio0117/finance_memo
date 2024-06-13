053c36c30e5907b858a78c690d3d8fade9c99921
milsio0117 <milsio117@gmail.com>
2024-06-13 23:16:17 +0900

    Stock登録、詳細表示の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "stocks#index"
+
+  resources :users, only: [:show, :edit, :update]
+  resources :stocks do
+
+    member do
+      patch :archive
+      patch :unarchive
+    end
+    collection do
+      get :history
+    end
+
+    resources :memos, only: [:create, :destroy]
+    member do
+      post 'add_image'
+      delete 'remove_image'
+    end
+    
+  end
+  
 end
